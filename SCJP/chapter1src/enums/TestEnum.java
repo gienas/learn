2f42b247594a2b3487d45c5478bae3df3cdbecfd
gienas <gienas@gmail.com>
2015-10-29 13:35:54 +0100

    ssss

--- SCJP/chapter1src/enums/TestEnum.java
+++ SCJP/chapter1src/enums/TestEnum.java
@@ -1,19 +1,42 @@
 package enums;
 
 enum Animals {
+
 	DOG("woof"), CAT("meow"), FISH("burble");
+
 	String sound;
 
 	Animals(String s) {
 		sound = s;
 	}
+	
+	public String getSound() {
+		return sound;
+	}
+	
+	
 }
 
 class TestEnum {
 	static Animals a;
 
 	public static void main(String[] args) {
-		System.out.println(a.DOG.ordinal());
-		System.out.println(a.DOG.sound + " " + a.FISH.sound);
+		//System.out.println(a.DOG.ordinal());
+		//System.out.println(a.DOG.sound + " " + a.FISH.sound);
+		
+		System.out.println(getValue("burble"));
 	}
+	
+	public static Animals getValue(String text) 
+	{
+		for ( Animals a: Animals.values())
+		{
+			if (text.equals(a.getSound()))
+			{
+				return a;
+			}	
+		}	
+		return null;
+	}
+	
 }
