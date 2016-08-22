0b219b0898d604cbb25f75cabba493f689622364
neugeeug <gienas@gmail.com>
2016-08-22 21:08:05 +0200

    Extend Super

---
+++ SCJP/chapter7src/TestExtendSuper.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestExtendSuper {
+
+  public static void main(String... strings) {
+
+      List<Animal> animals = new ArrayList<>();
+      List<Bird> birds = new ArrayList<>();
+      List<Bird> adlers = new ArrayList<>();
+      List<Swallow> swallow = new ArrayList<>();
+      List<Object> objects = new ArrayList<>(); //possible object is super of Bird
+      
+      add1(birds);
+      add1(adlers);
+      add1(swallow);
+      
+      add2(animals);
+      add2(birds);
+      add2(adlers);
+      add2(objects);
+  }
+
+  
+  
+  
+  public static void add1( List<? extends Bird> list) {
+    Adler ad = new Adler();
+    //list.add(ad); not possible because 
+    
+  }
+  
+  public static void add2( List<? super Bird> list) {
+    //list.add(new Animal()); not possible Animal is not Bird
+    list.add(new Bird());
+    list.add(new Adler());
+    list.add(new Swallow());
+    //list.add();
+  }
+  
+}
+
+class Animal {
+
+}
+
+class Bird extends Animal {
+
+}
+
+class Adler extends Bird {
+
+}
+
+class Swallow extends Bird {
+  
+} 
